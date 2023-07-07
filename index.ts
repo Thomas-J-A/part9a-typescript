f13d782b0fa36c66de08907265fe7b48704ae32a
Thomas Aspbury <tomaspbury@icloud.com>
2023-07-07 14:32:05 +0100

    Complete exercise 9.4

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express = require('express');
+
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
